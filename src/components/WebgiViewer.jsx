fc49dc07346a43c0c9ddb8f611aec04f11db4ffa
SESasanka <157799189+SESasanka@users.noreply.github.com>
2024-06-10 14:06:57 +0530

    main

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,93 @@
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+} from "react";
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  mobileAndTabletCheck,
+
+  // Color, // Import THREE.js internals
+  // Texture, // Import THREE.js internals
+} from "webgi";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { scrolllAnimatiion } from "../lib/scroll-animation";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function WebgiViewer() {
+  const canvasRef = useRef(null);
+
+  const memoizedScrollAnimation = useCallback((position, target, onUpdate) => {
+    if (position && target && onUpdate) {
+      scrolllAnimatiion(position, target, onUpdate);
+    }
+  }, []);
+
+  const setupViewer = useCallback(async () => {
+    const viewer = new ViewerApp({
+      canvas: canvasRef.current,
+    });
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+    const camera = viewer.scene.activeCamera;
+    const position = camera.position;
+    const target = camera.target;
+
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(true));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
+
+    viewer.renderer.refreshPipeline();
+
+    await manager.addFromPath("scene-black.glb");
+
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+    window.scrollTo(0, 0);
+
+    let needsUpdate = true;
+
+    const onUpdate = () => {
+      needsUpdate = true;
+      viewer.setDirty();
+    };
+
+    viewer.addEventListener("preFrame", () => {
+      if (needsUpdate) {
+        camera.positionTargetUpdated(true);
+        needsUpdate = false;
+      }
+    });
+    memoizedScrollAnimation(position, target, onUpdate);
+  }, []);
+
+  useEffect(() => {
+    setupViewer();
+  }, []);
+
+  return (
+    <div id="webgi-canvas-container">
+      <canvas id="webgi-canvas" ref={canvasRef} />
+    </div>
+  );
+}
+
+export default WebgiViewer;
